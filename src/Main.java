d9a53d7140db37a5657721a9d58f7a5ae6ad20cd
Dainius0820 <dainius0820@gmail.com>
2025-04-08 11:25:27 +0300

    COMPLETED TASKS 1 AND 2

--- src/Main.java
+++ src/Main.java
@@ -1,66 +1,87 @@
 public class Main {
     public static void main(String[] args) {
 
-        
+        System.out.println("\n-------------------------- TASK 1 --------------------------\n");
 
+        // 1. Sukurkite Funkciją kuri priima du int tipo kintamuosius. Juos
+        // susumuoja ir atspausdina.
 
+        printSum((int) (Math.random() * 10), (int) (Math.random() * 10));
 
+        System.out.println("\n-------------------------- TASK 2 --------------------------\n");
 
+        // 2. Sukurkite Funkciją kuri vadinasi PISq. Funkcija gražina double tipo
+        // reikšmę. Reikšmė yra : 9.8596; Gautą reikšmę atspausdinkite.
 
+        System.out.println(PISq());
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // END OF METHOD AND CLASS... DON'T DELETE!
     }
+    public static void printSum (int x, int y) {
+        System.out.println(x + y);
+    }
+
+    public static double PISq () {
+        return 9.8596;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // END OF CLASS... DON'T DELETE!}
 }
